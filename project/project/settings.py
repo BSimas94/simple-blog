8755ff097c5f1426f246accc09410d6e0c3cb0a6
Blake Simas <blakesimas@blakes-mbp.attlocal.net>
2017-06-26 15:02:23 -0700

    Changed database engine to postgresql

--- project/project/settings.py
+++ project/project/settings.py
@@ -76,11 +76,14 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'simple-blog',
+        'USER': 'postgres',
+        'PASSWORD': 'bsimas0992',
+        'HOST': '',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
